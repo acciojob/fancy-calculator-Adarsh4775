5afa34c02570eda13323fd9bd05fe9611d69bdc4
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-11-01 20:50:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function fancyCalc(a,b){
+    if(a >= 0 && b >= 0){
+	let multiNum = a * 2;
+	let total = multiNum + b;
+		return "The final result is: " + total;
+	}
+	else{
+		return "Enter a Valid Number in Both Inputs ";
+	}
+}
+const a = parseFloat(prompt("Enter the First Input"));
+const b = parseFloat(prompt("Enter the Second Input"));
+alert(fancyCalc(a,b));
